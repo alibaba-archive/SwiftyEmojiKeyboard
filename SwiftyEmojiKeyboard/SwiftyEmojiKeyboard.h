cef5ba4f0961dc1949cfc10562b092254b8e421f
HarriesChen <info@mrchenhao.com>
2016-04-28 16:23:37 +0800

    first commit

---
+++ SwiftyEmojiKeyboard/SwiftyEmojiKeyboard.h
@@ -0,0 +1,19 @@
+//
+//  SwiftyEmojiKeyboard.h
+//  SwiftyEmojiKeyboard
+//
+//  Created by ChenHao on 16/4/28.
+//  Copyright © 2016年 HarriesChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SwiftyEmojiKeyboard.
+FOUNDATION_EXPORT double SwiftyEmojiKeyboardVersionNumber;
+
+//! Project version string for SwiftyEmojiKeyboard.
+FOUNDATION_EXPORT const unsigned char SwiftyEmojiKeyboardVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SwiftyEmojiKeyboard/PublicHeader.h>
+
+
---
+++ SwiftyEmojiKeyboardDemo/SwiftyEmojiKeyboardDemo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyEmojiKeyboardDemo
+//
+//  Created by ChenHao on 16/4/28.
+//  Copyright © 2016年 HarriesChen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
