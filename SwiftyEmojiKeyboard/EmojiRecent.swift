92adf1ad4a5daa64c7b7b7f4adf2f4959ad37bad
liangmingzou@163.com <liangmingzou@163.com>
2016-09-21 17:56:52 +0800

    Support swift3.

--- SwiftyEmojiKeyboard/EmojiRecent.swift
+++ SwiftyEmojiKeyboard/EmojiRecent.swift
@@ -11,25 +11,25 @@ import Foundation
 class RecentManager {
     static var instance = RecentManager()
     
-    func insert(emoji: [String: String]) {
-        let userDefault = NSUserDefaults.standardUserDefaults()
+    func insert(_ emoji: [String: String]) {
+        let userDefault = UserDefaults.standard
         var recentEmojis = [[String: String]]()
-        if let recents = userDefault.objectForKey("recentsEmoji") as? [[String: String]] {
+        if let recents = userDefault.object(forKey: "recentsEmoji") as? [[String: String]] {
             recentEmojis = recents
         }
         
-        if let index = (recentEmojis.indexOf { $0["chs"] == emoji["chs"] }) {
-            recentEmojis.removeAtIndex(index)
+        if let index = (recentEmojis.index { $0["chs"] == emoji["chs"] }) {
+            recentEmojis.remove(at: index)
         }
         recentEmojis.append(emoji)
-        userDefault.setObject(recentEmojis, forKey: "recentsEmoji")
+        userDefault.set(recentEmojis, forKey: "recentsEmoji")
         userDefault.synchronize()
     }
     
     func getEmojis() -> [[String: String]] {
-        let userDefault = NSUserDefaults.standardUserDefaults()
-        if let recents = userDefault.objectForKey("recentsEmoji") as? [[String: String]] {
-            return recents.reverse()
+        let userDefault = UserDefaults.standard
+        if let recents = userDefault.object(forKey: "recentsEmoji") as? [[String: String]] {
+            return recents.reversed()
         }
         return []
     }
